e9c52b3afac9a4cf09e27536f60106f941ce687e
Santhosh <spinisetti6@gmail.com>
2023-11-10 10:41:26 +0530

    commit loader

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+var tl = gsap.timeline();
+function time(){
+    var a = 0;
+    setInterval(function(){
+        a=a + Math.floor(Math.random()*20);
+        if(a<100){
+            document.querySelector("#loader h1").innerHTML = a + "%";   
+        }
+        else{
+            a=100;
+            document.querySelector("#loader h1").innerHTML = a + "%"; 
+        }
+    },150);
+}
+// time();
+tl.to("#loader h1",{
+    scale:1.5,
+    delay:0.5,
+    duration:1,
+    onStart:time(),
+})
+tl.to("#loader", {
+    top:"-100vh",
+    delay:0.5,
+    duration:2,
+})
+
 gsap.to("#page1 h1",{
     transform:"translateX(-100%)",
     fontWeight:100,
